c77313dc16280923b882a7995997030eefd07f7c
deprekate <deprekate@gmail.com>
2019-09-16 13:02:25 -0700

    Fixed linking error

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+import os
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
-import os
 
 os.environ["CC"] = "g++"
 os.environ["CXX"] = "g++"
 #os.environ["CPP"] = "g++"
 #os.environ["LDSHARED"] = "g++"
 
-
 compile_args = ["-O3"]
+link_args    = ["-lstdc++"]
 
 repeatfinder_module = Extension('repeatfinder',
                     language='g++',
                     extra_compile_args=compile_args,
+                    extra_link_args=link_args,
                     include_dirs=[
                              '.',
                              '...',
@@ -22,6 +23,6 @@ repeatfinder_module = Extension('repeatfinder',
                     sources = ['src/repeatfinder.cpp'])
 
 setup (name = 'repeatfinder',
-       version = '1.1',
+       version = '1.2',
        description = 'A package for finding repeats in a DNA sequence',
        ext_modules = [repeatfinder_module])
