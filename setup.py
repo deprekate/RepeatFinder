61ba157c34560efbfb34ac0ea56683a2bc362526
deprekate <deprekate@gmail.com>
2019-10-14 14:36:32 -0700

    Updating metadata

--- setup.py
+++ setup.py
@@ -23,8 +23,24 @@ repeatfinder_module = Extension('repeatfinder',
                     library_dirs = [os.getcwd(),],
                     sources = ['src/repeatfinder.cpp'])
 
-setup (name = 'repeatfinder',
-       version = '1.4',
-       description = 'A package for finding repeats in a DNA sequence',
-       python_requires='>3.5.2',
-       ext_modules = [repeatfinder_module])
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+
+
+setup (
+    name = 'repeatfinder',
+    version = '1.4',
+    author = "Katelyn McNair",
+    author_email = "deprekate@gmail.com",
+    description = 'A package for finding repeats in a DNA sequence',
+    long_description = long_desc,
+    long_description_content_type="text/markdown",
+    url =  "https://github.com/deprekate/RepeatFinder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.5.2',
+    ext_modules = [repeatfinder_module]
+)
