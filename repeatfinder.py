7db6cc54f0a65cc5905868609807ebddaefb1c6c
deprekate <deprekate@gmail.com>
2022-03-09 15:35:19 -0800

    Added help menu

--- repeatfinder.py
+++ repeatfinder.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL) 
+import os
 import sys
+import argparse
+from argparse import RawTextHelpFormatter
 
 sys.path.pop(0)
 import repeatfinder as rf
 from genbank.file import File
 from genbank.locus import Locus
 
+def is_valid_file(x):
+	if not os.path.exists(x):
+		raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+	return x
 
 def is_in(repeats, repeat):
 	print(repeat)
@@ -16,23 +23,29 @@ def is_in(repeats, repeat):
 		return True
 	return False
 
-repeats = dict()
+if __name__ == '__main__':
+	usage = '%s [-opt1, [-opt2, ...]] infile' % __file__
+	parser = argparse.ArgumentParser(description='', formatter_class=RawTextHelpFormatter, usage=usage)
+	parser.add_argument('infile', type=is_valid_file, help='input file')
+	parser.add_argument('-l', '--len', help='The minimum length for repeats', type=int, default=15)
+	parser.add_argument('-o', '--outfile', action="store", default=sys.stdout, type=argparse.FileType('w'), help='where to write output [stdout]')
+	args = parser.parse_args()
 
-header = ''
-dna = ""
-with open(sys.argv[1]) as fp:
-	for line in fp:
-		if not line.startswith(">"):
-			dna += line.rstrip().replace('N','')
-		elif dna:
-			repeats = rf.get_repeats(dna)
-			header = line[1:].rstrip()
-			locus = Locus(header, dna)
-			for repeat in repeats:
-				if repeat[1] - repeat[0] > 12:
-					a,b,c,d = map(str, repeat)
-					pairs = ((a,b),(c,d))
-					locus.add_feature('repeat_region', 0, pairs)
-			locus.write()
-			dna = ""
-
+	header = ''
+	dna = ""
+	with open(args.infile) as fp:
+		for line in fp:
+			if not line.startswith(">"):
+				dna += line.rstrip().replace('N','').replace('n','')
+			elif dna:
+				repeats = rf.get_repeats(dna)
+				header = line[1:].rstrip()
+				locus = Locus(header, dna)
+				for repeat in repeats:
+					if repeat[1] - repeat[0] > args.len:
+						a,b,c,d = map(str, repeat)
+						pairs = ((a,b),(c,d))
+						locus.add_feature('repeat_region', 0, pairs)
+				locus.write()
+				dna = ""
+	
